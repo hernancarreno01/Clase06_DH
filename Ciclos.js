ec999e60354c3e9b39ca2b54febd4712a7a4286c
hernancarreno01 <radioportatil@gmail.com>
2023-06-18 22:56:21 -0300

    Actividades

---
+++ Ciclos.js
@@ -0,0 +1,34 @@
+/*for (let i = 1; i <= 5; i = i*2) { // Multiplicando en el modificador
+    console.log(i)
+}
+
+for (let i = 0; i <= 5 || i != 4; i++) { // Bucle infinito, cuidado con el OR
+    console.log(i)
+}
+
+for (let i = 0; i <= 5 && i != 4; i++) { // Utilizando operadores logicos
+    console.log(i)
+}
+
+for (let i = 0; i <= 5; i++) { // Ciclo for común
+    console.log(i)
+}
+
+for (let i = 14; i <= 5; i++) { // No entra en la condición
+    console.log(i)
+}
+
+
+for (let i = 1; i <= 5;) { // Sin modificador bucle infinito
+    console.log(i)
+}
+*/
+let i = 0
+/*do{
+    console.log("Console.log del Do")
+    i = i + 1
+}*/
+while (i <= 5) {
+    console.log("Finalicé la ejecución")
+    i++
+}
